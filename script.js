07143294464dbdaeaee1a4e946af9cdeed27da73
PGreen85 <polinagreen85@gmail.com>
2024-09-02 16:30:34 +0200

    Update all files

---
+++ script.js
@@ -0,0 +1,31 @@
+let i = 1;
+    for(let li of carousel.querySelectorAll('li')) {
+      li.style.position = 'relative';
+      li.insertAdjacentHTML('beforeend', `<span style="position:absolute;left:0;top:0">${i}</span>`);
+      i++;
+    }
+
+    /* config */
+    let width = 130; // image width
+    let count = 3; // how many images are visible
+
+    let list = carousel.querySelector('ul');
+    let listElems = carousel.querySelectorAll('li');
+
+    let position = 0; // scroll position
+
+    carousel.querySelector('.prev').onclick = function() {
+      // shift left
+      position += width * count;
+      // последнее передвижение влево может быть не на 3, а на 2 или 1 элемент
+      position = Math.min(position, 0)
+      list.style.marginLeft = position + 'px';
+    };
+
+    carousel.querySelector('.next').onclick = function() {
+      // shit right
+      position -= width * count;
+      // последнее передвижение вправо может быть не на 3, а на 2 или 1 элемент
+      position = Math.max(position, -width * (listElems.length - count));
+      list.style.marginLeft = position + 'px';
+    };
